11bf778db49e897ff5c4f3fbb2ffc3db6d7426db
naska0link <tekkitcreeper@gmail.com>
2022-04-22 17:40:48 -0700

    database

---
+++ files/script.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS userbalance (
+    UserID text PRIMARY KEY,
+    UserName text,
+    MessagesSent integer DEFAULT 0,
+    Coins integer DEFAULT 0,
+    Gems integer DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS coincooldowns (
+    UserID text PRIMARY KEY,
+    UserName text
+);
+CREATE TABLE IF NOT EXISTS gemcooldowns (
+    UserID text PRIMARY KEY,
+    UserName text
+);
+CREATE TABLE IF NOT EXISTS shop (
+    ItemName text PRIMARY KEY,
+    ItemCost integer,
+    Currency text
+);
+CREATE TABLE IF NOT EXISTS reactcommands (
+    CommandName text PRIMARY KEY,
+    CommandMessage text
+);
